30a82db8210d0fd9afa8393bea9a85c1d5ef3787
Jakub Peterka <jakub@Mac.lab130>
2026-03-25 16:19:39 +0100

    another turn test

--- robolab_turtlebot/MAIN/turn_test.py
+++ robolab_turtlebot/MAIN/turn_test.py
@@ -3,7 +3,15 @@ from robolab_turtlebot import Turtlebot, Rate, get_time
 t = Turtlebot()
 r = Rate(10)
 
-_, _, a0 = t.get_odometry()
+# počkej, až bude odometrie dostupná
+t.wait_for_odometry()
+
+odo0 = t.get_odometry()
+if odo0 is None:
+    print("Odometry is None (not ready).")
+    exit(1)
+
+a0 = odo0[2]
 
 t0 = get_time()
 while get_time() - t0 < 1.0:
@@ -11,5 +19,10 @@ while get_time() - t0 < 1.0:
     r.sleep()
 t.cmd_velocity(0.0, 0.0)
 
-_, _, a1 = t.get_odometry()
+odo1 = t.get_odometry()
+if odo1 is None:
+    print("Odometry is None after turning.")
+    exit(1)
+
+a1 = odo1[2]
 print("a0=", a0, "a1=", a1, "delta=", a1 - a0)
