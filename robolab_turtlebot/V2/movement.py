2909f5d2a648184bc71adff40254d8ad0b5c518a
bejgin <bejgin@smida.cz>
2026-04-15 20:35:56 +0200

    28 cm

--- robolab_turtlebot/V2/movement.py
+++ robolab_turtlebot/V2/movement.py
@@ -243,7 +243,7 @@ def rotate_left_10deg_and_drive_30cm(turtle, stop_requested=None):
     ok = drive_around.drive_straight(
         turtle,
         rate,
-        dist_m=0.25,
+        dist_m=0.28,
         v=0.15,
         stop_requested=stop_requested,
     )
